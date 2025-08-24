25d12a6b604d0c3361d2d7d58d4156aeab6a547f
Eyob-bit <eyobbehailu33@gmail.com>
2025-08-24 16:48:53 +0300

    I updated the function which is insert_at_specific_place()

--- StudentApp.cpp
+++ StudentApp.cpp
@@ -1,134 +1,212 @@
-#include<iostream> 
+#include <iostream>
 
 using namespace std;
 
-struct student {
+struct student
+{
     int studentID;
     string studentName;
     student *next;
 };
-void insertFront();
-void insertAtEnd();
-void add_Student();
-void delete_Student();
-void viewAll();
 
 student s;
 student *head = NULL;
-void menu() {
-    cout<<"1. Add student"<<endl
-    <<"2. Delete a student"<<endl
-    <<"3. View All students"<<endl
-    <<"4. Exit"<<endl<<endl;
+
+void menu()
+{
+    cout << "1. Add student" << endl
+         << "2. Delete a student" << endl
+         << "3. View All students" << endl
+         << "4. Exit" << endl
+         << endl;
 }
 
-void insertFront() {
+void insertFront()
+{
     student *temp = new student;
-    cout <<"Enter your ID ";
-    cin>>s.studentID;
-    cout <<"Enter your Name ";
-    cin>>s.studentName;
-    
+    cout << "Enter the ID of the Student ";
+    cin >> s.studentID;
+    cout << "Enter the Name of the Student ";
+    cin >> s.studentName;
+
     temp->next = head;
     temp->studentID = s.studentID;
     temp->studentName = s.studentName;
-    head = temp;
-}
 
-void insertAtEnd() {
-        cout <<"Enter your ID ";
-        cin>>s.studentID;
-        cout <<"Enter your Name ";
-        cin>>s.studentName;
-    
-        student *temp = new student;
+    if (head == NULL)
+    {
+        head = temp;
+        cout << " Successfully Inserted..." << endl;
+    }
+    else
+    {
+        temp->next = head;
         temp->studentID = s.studentID;
         temp->studentName = s.studentName;
-        temp->next = NULL;
-
-        if(head == NULL) {
-            head = temp;
-        }
-        else {
-            student *temp2;
-            temp2 = head;
-            while(temp2->next != NULL) {
-                temp2 = temp2->next;
-            }
-            temp2->next = temp;
-        }
+        head = temp;
+        cout << " Successfully Inserted..." << endl;
+    }
 }
-void add_Student() {
+
+void insert_at_specific_place()
+{
+    int targetID;
+    cout << "Enter the student ID after which you want to insert: ";
+    cin >> targetID;
+    cout << "Enter the ID of the Student ";
+    cin >> s.studentID;
+    cout << "Enter the Name of the Student ";
+    cin >> s.studentName;
+
+    student *temp = new student;
+    temp->next = head;
+    temp->studentID = s.studentID;
+    temp->studentName = s.studentName;
+
+    if (head == NULL)
+    {
+        head = temp;
+        cout << " Successfully Inserted..." << endl;
+    }
+    else
+    {
+        student *finder = head;
+        while (targetID != finder->studentID)
+        {
+            if (finder == NULL)
+            {
+                cout << "There is no student registered with this id, make sure the id is correct " << endl;
+                return;
+            }
+            finder = finder->next;
+        }
+        temp->next = finder->next;
+        finder->next = temp;
+        cout << " Successfully Inserted..." << endl;
+    }
+}
+
+void insertAtEnd()
+{
+    cout << "Enter the ID of the Student ";
+    cin >> s.studentID;
+    cout << "Enter the Name of the Student ";
+    cin >> s.studentName;
+
+    student *temp = new student;
+    temp->studentID = s.studentID;
+    temp->studentName = s.studentName;
+    temp->next = NULL;
+
+    if (head == NULL)
+    {
+        head = temp;
+    }
+    else
+    {
+        student *temp2;
+        temp2 = head;
+        while (temp2->next != NULL)
+        {
+            temp2 = temp2->next;
+        }
+        temp2->next = temp;
+    }
+}
+
+void add_Student()
+{
     int ch;
-    cout <<"Are you adding at the front or at the end, (1 for front and 2 for end)-> ";
-    cin >>ch;
+    cout << "Where do you want to insert?" << endl;
+    cout << "1. At the beginning" << endl;
+    cout << "2. After a specific student's ID" << endl;
+    cout << "3. At the end" << endl;
+    cin >> ch;
 
-    if(ch == 1) {
+    switch (ch)
+    {
+    case 1:
         insertFront();
-    }
-    else if(ch == 2) {
+        break;
+    case 2:
+        insert_at_specific_place();
+        break;
+    case 3:
         insertAtEnd();
-    }
-    else {
-        cout <<"Wrong choice choose again:"<<endl;
+        break;
+    default:
+        cout << "Wrong choice, choose again" << endl;
         add_Student();
+        break;
     }
-} 
+}
 
-void delete_Student() {
+void delete_Student()
+{
     int id;
     student *temp = head;
-    cout <<"Enter an ID: ";
-    cin >>id;
-    while(temp->next != NULL) {
-        if(id = temp->studentID) {
+    cout << "Enter Student's ID to delete: ";
+    cin >> id;
+    while (temp->next != NULL)
+    {
+        if (id = temp->studentID)
+        {
             temp->next = temp->next->next;
         }
         temp = temp->next;
     }
 }
 
-void viewAll() {
+void viewAll()
+{
     student *temp = head;
-    if (temp == NULL) {
+    if (temp == NULL)
+    {
         cout << "there is no data!!!" << endl;
         return;
     }
-    cout << "ID     Name" << endl;
-    while (temp != NULL) {
-        cout << temp->studentID << "   " << temp->studentName << endl;
+    cout << "________________________" << endl;
+    cout << "ID  |  Name" << endl;
+    while (temp != NULL)
+    {
+        cout << temp->studentID << " | " << temp->studentName << endl;
+        cout << "___________________" << endl;
         temp = temp->next;
     }
 }
 
-int main() {
+int main()
+{
     menu();
     int choice;
-    cout <<"Enter your choice ";
-    cin >>choice;
+    cout << "Enter your choice ";
+    cin >> choice;
 
-    while(true) {
-        if(choice == 4) {
-            cout<<"Exiting...";
+    while (true)
+    {
+        if (choice == 4)
+        {
+            cout << "Exiting...";
             break;
         }
-        switch(choice) {
-            case 1:
+        switch (choice)
+        {
+        case 1:
             add_Student();
             break;
-            case 2:
+        case 2:
             delete_Student();
             break;
-            case 3:
+        case 3:
             viewAll();
             break;
-            default:
-            cout <<"wrong choice";
+        default:
+            cout << "wrong choice";
             break;
         }
         menu();
-        cout <<"Enter your choice ";
-        cin >>choice;
+        cout << "Enter your choice ";
+        cin >> choice;
     }
     return 0;
 }
